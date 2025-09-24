0a14d02a46b6e988be7f3f7fe112edd428af1638
Santhakumari <n.santhakumari@house-of-communication.com>
2025-09-24 22:41:17 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import {Routes, Route} from 'react-router-dom'
-import Main from './components/main'
 import EmployeeForm from './components/EmployeeForm'
+import Main from './components/main'
 
 function App() {
   return (
